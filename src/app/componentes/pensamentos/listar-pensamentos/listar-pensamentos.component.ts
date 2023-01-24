fa9b4da4cb9458f2f6f4020e2223f44dc69262d3
Danielscript1 <danielsousagit@gmail.com>
2023-01-24 15:46:59 -0300

    projeto Angular 14

---
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,20 @@
+import { PensamentoService } from './../pensamento.service';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.css'],
+})
+export class ListarPensamentosComponent implements OnInit {
+  listaPensamentos: Pensamento[] = [];
+
+  constructor(private service: PensamentoService) {}
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamento) => {
+      this.listaPensamentos = listaPensamento;
+    });
+  }
+}
